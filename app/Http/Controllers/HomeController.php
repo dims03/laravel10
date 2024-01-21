22054f7b6c0dab9d02e02423d1560edc3136d8e2
DenDims <dimasp95@icloud.com>
2024-01-21 10:34:51 +0800

    edit datas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,5 +40,32 @@ class HomeController extends Controller
         User::create($data);
         return redirect()->route('index');
     }
+
+    public function edit(Request $request,$id){
+        $data = User::find($id);
+        return view('edit', compact('data'));
+        // dd($data);
+    }
+
+    public function update(Request $request,$id){
+        // dd($request->all());
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+             'nama' => 'required',
+             'password' => 'nullable',
+         ]);
+ 
+         if($validator->fails()) return redirect()->back()->withInput()->withErrors($validator);
+ 
+         $data['email'] = $request->email;
+         $data['name'] = $request->nama;
+         if($request->password){
+             $data['password'] = Hash::make($request->password);
+         }
+
+         User::whereId($id)->update($data);
+         
+         return redirect()->route('index');
+    }
 }
 
