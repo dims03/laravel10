fee5cca6aa695e4b668475a1badd938d077cfdde
DenDims <dimasp95@icloud.com>
2024-01-21 11:04:04 +0800

    navbar routes

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -207,7 +207,7 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
+            <a href="/" class="nav-link">
               <i class="nav-icon fas fa-home"></i>
               <p>
                 Dashboard
@@ -215,7 +215,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
+            <a href="{{ route('index') }}" class="nav-link">
               <i class="nav-icon fas fa-user"></i>
               <p>
                 User
