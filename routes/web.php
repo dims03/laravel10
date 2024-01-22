27945f46603acbd9dba5e2af4b2068ab682ce963
DenDims <dimasp95@icloud.com>
2024-01-22 20:23:08 +0800

    login layout

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/login',[HomeController::class, 'login'])->name('login');
+Route::get('/login',[LoginController::class, 'index'])->name('login');
+Route::post('/login-proses',[LoginController::class, 'login_proses'])->name('login-proses');
 Route::get('/',[HomeController::class, 'dashboard'])->name('dashboard');
 
 Route::get('/user',[HomeController::class, 'index'])->name('index');
