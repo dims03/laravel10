8810b3e74b931cdf00be1d8713d815280f11b7c5
DenDims <dimasp95@icloud.com>
2024-01-22 20:08:54 +0800

    login route & prepare login controller

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/',[HomeController::class, 'dashboard']);
+Route::get('/login',[HomeController::class, 'login'])->name('login');
+Route::get('/',[HomeController::class, 'dashboard'])->name('dashboard');
+
 Route::get('/user',[HomeController::class, 'index'])->name('index');
 Route::get('/create',[HomeController::class, 'create'])->name('user.create');
 Route::post('/store',[HomeController::class, 'store'])->name('user.store');
-Route::get('/edit/{id}',[HomeController::class, 'edit'])->name('user.edit');
+
 Route::get('/edit/{id}',[HomeController::class, 'edit'])->name('user.edit');
 Route::put('/update/{id}',[HomeController::class, 'update'])->name('user.update');
 Route::delete('/delete/{id}',[HomeController::class, 'delete'])->name('user.delete');
