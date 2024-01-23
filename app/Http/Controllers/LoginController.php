fa6df49f43d7a0f9fea7bbbf3b414f84d77b48e3
DenDims <dimasp95@icloud.com>
2024-01-23 09:23:51 +0800

    regist route & register form

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,36 @@ class LoginController extends Controller
         // dd("tes page");
         Auth::logout();
         return redirect()->route('login')->with('success', 'Anda berhasil logout');
+    }
+
+    public function register(){
+        return view('auth.register');
+    }
+
+    public function register_proses(Request $request){
+        // dd($request->all());
+        $request->validate([
+            'nama' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ]);
+
+        $data['name'] = $request->nama;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+
+        User::create($data);
+
+        $login = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if(Auth::attempt($login)){
+            return redirect()->route('admin.dashboard');
+        }
+        else {
+            return redirect()->route('login')->with('failed', 'Email atau Password Salah.');
+        }
     }
 }
